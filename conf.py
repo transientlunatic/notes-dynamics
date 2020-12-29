629cd65a8543a85d8ab735db08608ff4e8a1ca6e
Daniel Williams <mail@daniel-williams.co.uk>
2020-12-29 12:23:56 +0000

    Removed the static path from the conf file.

--- conf.py
+++ conf.py
@@ -63,7 +63,6 @@ html_theme = 'kentigern'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
 mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
 mathjax_config = {
